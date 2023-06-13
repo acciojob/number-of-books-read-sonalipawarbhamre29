9dd8509e897ba62213ee566432823d3d18666009
sonalipawar <124123724+sonalipawarbhamre29@users.noreply.github.com>
2023-06-13 18:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,16 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	    // write your code here
+    let count=0;
+      library.forEach((i)=>{
+       if(i.readingStatus===true)
+       {
+         count++;
+       }
+      })
+      return count;
+
 };
 
 // Do not change the code below
